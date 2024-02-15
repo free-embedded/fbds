af3a62ccb4c32eee7458004b9f4129ce3390c8d3
Congiuntivo <simone.decarli@studenti.unitn.it>
2024-02-15 13:50:41 +0100

    Servo1 rotation and stop

--- src/main.c
+++ src/main.c
@@ -10,7 +10,45 @@ Graphics_Context g_sContext;
 /* ADC results buffer */
 static uint16_t resultsBuffer[2];
 
-void _adcInit() {
+#define map(x, in_min, in_max, out_min, out_max) \
+    ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+
+
+/* Timer_A Compare Configuration Parameter  (PWM) */
+Timer_A_CompareModeConfig compareConfig_PWM = {
+    TIMER_A_CAPTURECOMPARE_REGISTER_3,        // Use CCR3
+    TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE, // Disable CCR interrupt
+    TIMER_A_OUTPUTMODE_RESET_SET,             // Toggle output but
+    375                                       // 1.5 ms pulse width
+};
+
+/* Timer_A Up Configuration Parameter */
+const Timer_A_UpModeConfig upConfig = {
+    TIMER_A_CLOCKSOURCE_SMCLK,           // SMCLK = 3 MhZ
+    TIMER_A_CLOCKSOURCE_DIVIDER_12,      // SMCLK/12 = 250 KhZ
+    5000,                                // 20 ms tick period
+    TIMER_A_TAIE_INTERRUPT_DISABLE,      // Disable Timer interrupt
+    TIMER_A_CCIE_CCR0_INTERRUPT_DISABLE, // Disable CCR0 interrupt
+    TIMER_A_DO_CLEAR                     // Clear value
+};
+
+void _servoInit()
+{
+    // Configures P2.6 to PM_TA0.3 for using Timer PWM to control Servo1
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN6,
+                                                GPIO_PRIMARY_MODULE_FUNCTION);
+
+    // Configuring Timer_A0 for Up Mode and starting
+    Timer_A_configureUpMode(TIMER_A0_BASE, &upConfig);
+    Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
+
+    // Initialize compare registers to generate PWM  for the Servo1 Port
+    Timer_A_initCompare(TIMER_A0_BASE, &compareConfig_PWM); // For P2.6
+
+}
+
+void _adcInit()
+{
     /* Configures Pin 6.0 and 4.4 as ADC input */
     GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P6, GPIO_PIN0, GPIO_TERTIARY_MODULE_FUNCTION);
     GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P4, GPIO_PIN4, GPIO_TERTIARY_MODULE_FUNCTION);
@@ -20,18 +58,18 @@ void _adcInit() {
     ADC14_initModule(ADC_CLOCKSOURCE_ADCOSC, ADC_PREDIVIDER_64, ADC_DIVIDER_8, 0);
 
     /* Configuring ADC Memory (ADC_MEM0 - ADC_MEM1 (A15, A9)  with repeat)
-         * with internal 2.5v reference */
+     * with internal 2.5v reference */
     ADC14_configureMultiSequenceMode(ADC_MEM0, ADC_MEM1, true);
     ADC14_configureConversionMemory(ADC_MEM0,
-        ADC_VREFPOS_AVCC_VREFNEG_VSS,
-        ADC_INPUT_A15, ADC_NONDIFFERENTIAL_INPUTS);
+                                    ADC_VREFPOS_AVCC_VREFNEG_VSS,
+                                    ADC_INPUT_A15, ADC_NONDIFFERENTIAL_INPUTS);
 
     ADC14_configureConversionMemory(ADC_MEM1,
-        ADC_VREFPOS_AVCC_VREFNEG_VSS,
-        ADC_INPUT_A9, ADC_NONDIFFERENTIAL_INPUTS);
+                                    ADC_VREFPOS_AVCC_VREFNEG_VSS,
+                                    ADC_INPUT_A9, ADC_NONDIFFERENTIAL_INPUTS);
 
-/* Enabling the interrupt when a conversion on channel 1 (end of sequence)
- *  is complete and enabling conversions */
+    /* Enabling the interrupt when a conversion on channel 1 (end of sequence)
+     *  is complete and enabling conversions */
     ADC14_enableInterrupt(ADC_INT1);
 
     /* Enabling Interrupts */
@@ -48,7 +86,8 @@ void _adcInit() {
     ADC14_toggleConversionTrigger();
 }
 
-void _graphicsInit() {
+void _graphicsInit()
+{
     /* Initializes display */
     Crystalfontz128x128_Init();
 
@@ -63,15 +102,15 @@ void _graphicsInit() {
     Graphics_clearDisplay(&g_sContext);
 
     Graphics_drawStringCentered(&g_sContext,
-        (int8_t*)"Joystick:",
-        AUTO_STRING_LENGTH,
-        64,
-        30,
-        OPAQUE_TEXT);
-
+                                (int8_t *)"Joystick:",
+                                AUTO_STRING_LENGTH,
+                                64,
+                                30,
+                                OPAQUE_TEXT);
 }
 
-void _hwInit() {
+void _hwInit()
+{
     /* Halting WDT and disabling master interrupts */
     WDT_A_holdTimer();
     Interrupt_disableMaster();
@@ -92,32 +131,35 @@ void _hwInit() {
 
     _graphicsInit();
     _adcInit();
+    _servoInit();
 }
 
 /*
  * Main function
  */
-int main(void) {
+int main(void)
+{
     _hwInit();
 
-
-    while (1) {
+    while (1)
+    {
         PCM_gotoLPM0();
     }
 }
 
-
 /* This interrupt is fired whenever a conversion is completed and placed in
  * ADC_MEM1. This signals the end of conversion and the results array is
  * grabbed and placed in resultsBuffer */
-void ADC14_IRQHandler(void) {
+void ADC14_IRQHandler(void)
+{
     uint64_t status;
 
     status = ADC14_getEnabledInterruptStatus();
     ADC14_clearInterruptFlag(status);
 
     /* ADC_MEM1 conversion completed */
-    if (status & ADC_INT1) {
+    if (status & ADC_INT1)
+    {
         /* Store ADC14 conversion results */
         resultsBuffer[0] = ADC14_getResult(ADC_MEM0);
         resultsBuffer[1] = ADC14_getResult(ADC_MEM1);
@@ -125,31 +167,34 @@ void ADC14_IRQHandler(void) {
         char string[10];
         sprintf(string, "X: %5d", resultsBuffer[0]);
         Graphics_drawStringCentered(&g_sContext,
-            (int8_t*)string,
-            8,
-            64,
-            50,
-            OPAQUE_TEXT);
+                                    (int8_t *)string,
+                                    8,
+                                    64,
+                                    50,
+                                    OPAQUE_TEXT);
 
         sprintf(string, "Y: %5d", resultsBuffer[1]);
         Graphics_drawStringCentered(&g_sContext,
-            (int8_t*)string,
-            8,
-            64,
-            70,
-            OPAQUE_TEXT);
+                                    (int8_t *)string,
+                                    8,
+                                    64,
+                                    70,
+                                    OPAQUE_TEXT);
 
-/* Determine if JoyStick button is pressed */
+        // Determine if JoyStick button is pressed
         int buttonPressed = 0;
         if (!(P4IN & GPIO_PIN1))
+        {
             buttonPressed = 1;
+            Timer_A_stopTimer(TIMER_A0_BASE);
+        }
 
         sprintf(string, "Button: %d", buttonPressed);
         Graphics_drawStringCentered(&g_sContext,
-            (int8_t*)string,
-            AUTO_STRING_LENGTH,
-            64,
-            90,
-            OPAQUE_TEXT);
+                                    (int8_t *)string,
+                                    AUTO_STRING_LENGTH,
+                                    64,
+                                    90,
+                                    OPAQUE_TEXT);
     }
 }
